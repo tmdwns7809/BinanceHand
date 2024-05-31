6ae9c1dc0d63451df4f492bdc24b0edba3923d1e
sj <tmdwns7809@gmail.com>
2024-05-31 12:23:54 +0900

    다른 테스트

--- BinanceHand/Form1.cs
+++ BinanceHand/Form1.cs
@@ -83,9 +83,7 @@ namespace BinanceHand
             FormClosed += Form1_FormClosed;
             Load += Form1_Load;
 
-            //Trading.instance = new Trading(this, Settings.ProgramBinanceFutures, 8.4m, 20);
-            //Trading.instance = new Trading(this, Settings.ProgramBinanceFutures, 8.43m, 20);
-            Trading.instance = new Trading(this, Settings.ProgramBinanceFutures, 8.41m, 20);
+            Trading.instance = new Trading(this, Settings.ProgramBinanceFutures, 8.412m, 20);
             Trading.instance.HoONandOFF += Trading_HoONandOFF;
             Trading.instance.AggONandOFF += Trading_AggONandOFF;
             Trading.instance.ShowChartAdditional += Trading_ShowChartAdditional;
